147c8a77f87220be839c55091cea2772918a2a0e
Yaroslav Zhuravlov <yaroslav.zhuravlov.dev@gmail.com>
2025-06-05 15:04:43 +0300

    add get_token_user_id, get_current_user, get_current_user_if_active

--- src/security/http.py
+++ src/security/http.py
@@ -1,4 +1,12 @@
-from fastapi import Request, HTTPException, status
+from fastapi import Depends, Request, HTTPException, status
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config.dependencies import get_jwt_auth_manager
+from database.models.accounts import UserModel
+from exceptions.security import BaseSecurityError
+from security.interfaces import JWTAuthManagerInterface
+from database import get_db
 
 
 def get_token(request: Request) -> str:
@@ -14,7 +22,7 @@ def get_token(request: Request) -> str:
     if not authorization:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Authorization header is missing"
+            detail="Authorization header is missing",
         )
 
     scheme, _, token = authorization.partition(" ")
@@ -22,7 +30,58 @@ def get_token(request: Request) -> str:
     if scheme.lower() != "bearer" or not token:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid Authorization header format. Expected 'Bearer <token>'"
+            detail="Invalid Authorization header format. Expected 'Bearer <token>'",
         )
 
     return token
+
+
+async def get_token_user_id(
+    token: str = Depends(get_token),
+    jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+) -> int:
+    try:
+        payload = jwt_manager.decode_access_token(token)
+        user_id = payload.get("user_id")
+
+        if not user_id or not isinstance(user_id, int):
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Bad payload in Bearer token",
+            )
+
+        return user_id
+    except BaseSecurityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e)
+        )
+
+
+async def get_current_user(
+    db: AsyncSession = Depends(get_db),
+    token_user_id: int = Depends(get_token_user_id),
+) -> UserModel:
+
+    current_user = await db.scalar(
+        select(UserModel).where(UserModel.id == token_user_id)
+    )
+
+    if current_user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found or not active.",
+        )
+
+    return current_user
+
+
+async def get_current_user_if_active(
+    current_user: UserModel = Depends(get_current_user),
+) -> UserModel:
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found or not active.",
+        )
+
+    return current_user
